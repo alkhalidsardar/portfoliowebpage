7130a0d58c19de59e2974b186c108de99bf411fc
khalid <awfulgaming997@gmail.com>
2023-01-31 18:57:05 +0530

    k

---
+++ script.js
@@ -0,0 +1,23 @@
+var tabtitlesvariable = document.getElementsByClassName('tabtitles');
+var tabcontentvariable = document.getElementsByClassName('tabcontent');
+
+/* creating function */
+function opentab(tabname){
+for(tabtitles of tabtitlesvariable) {
+    tabtitles.classList.remove("activelinks");  
+}
+for(tabcontent of tabcontentvariable) {
+    tabcontent.classList.remove("activetab");  
+}
+
+document.getElementById(tabname).classList.add("activetab");
+}
+
+/*------------------------------------------------------navbar------------------------------------------------ -*/
+var navicons = document.getElementById("navicons");
+function showMenu() {
+navicons.style.right = "0";
+}
+function hideMenu() {
+navicons.style.right = "-200px";
+}
